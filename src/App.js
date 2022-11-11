a5225f4baf4688ca87d9943da98bc29b8aacbe5f
Art Beckett <89126163+Bear1986@users.noreply.github.com>
2022-11-11 15:29:33 +0000

    Star Wars search with robots

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 // import { render } from "@testing-library/react";
 import React, { Component } from "react";
 import CharacterInfo from "./CharacterInfo";
-import { characters } from "./Characters";
+// import { characters } from "./Characters";
 import SearchBox from "./SearchBox";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      characters: characters,
+      characters: [],
+      // characters: characters,
       searchfield: "",
     };
+    // console.log('constructor')
   }
+
+  componentDidMount() {
+    // this.setState({ characters: characters })
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState({ characters: users });
+      });
+    // console.log('render did mount')
+  }
+
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value });
+    // console.log(event.target.value);
   };
   render() {
     const filteredCharacters = this.state.characters.filter((character) => {
@@ -21,16 +35,24 @@ class App extends Component {
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
-    return (
+    if (this.state.characters.length === 0) {
+      return (
+        <h1 className="text-7xl font-black decoration-1 text-green-400">
+          Loading...
+        </h1>
+      );
+    } else {
+      return (
+    // console.log('render ')
       <div className="tc">
         <h1 className="text-7xl font-black decoration-1 text-green-400">
-          Star Wars Friends
+          Friends
         </h1>
         <SearchBox searchChange={this.onSearchChange} />
         <CharacterInfo characters={filteredCharacters} />
       </div>
     );
+    }
   }
 }
-
 export default App;
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-const SearchBox = ({searchfield, searchChange}) => {
+const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className="p-4 flex justify-center">
       <input
-           className="pa3 ba b--green bg-lightest-blue rounded-full shadow-5 w-[24rem] h-[4.5rem] outline-none"
-           type="search"
-           placeholder="Search Star Wars Friends"
-           onChange={searchChange}
+        className="pa3 ba b--green bg-lightest-blue rounded-full shadow-5 w-[24rem] h-[4.5rem] outline-none"
+        type="search"
+        placeholder="Search Friends"
+        onChange={searchChange}
       />
     </div>
   );
